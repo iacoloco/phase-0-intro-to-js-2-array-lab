01043796869aa503b6b2305f81661363cb369bef
Iachini <iachiniarmndo@gmail.com>
2022-12-21 22:37:24 +0000

    commit tree b3d6c0c7cc8da30eba50350b706eb33a0911a7f3

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+// Write your solution here
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    return [...cats,name]
+}
+
+
+function prependCat(name){
+    return [name,...cats]
+}
+//1) index.js
+//Array functions
+//removeLastCat()
+  //removes the last cat in the cats array and returns a new array, leaving the cats array unchanged:
+
+  function removeLastCat(){
+    return cats.slice(0,2)
+  }
+  
+  function removeFirstCat(){
+    return cats.slice(1,cats.length)
+  }
+
+  console.log(removeFirstCat());
